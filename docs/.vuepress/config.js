19791cd402eb461efa14f9c3e486727e57f3e4cd
pq <68978372+pq-code@users.noreply.github.com>
2022-11-22 14:26:42 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'P-UI',
+    description: 'Vu3ui',
+    themeConfig: {
+        nav: [
+            { text: "首页", link: "/" },
+            { text: "基础教程", link: "/componentDoc/pTabel" },
+        ],
+        sidebar: [
+            '/',
+            '/componentDoc/pTabel'
+        ]
+    }
+}
